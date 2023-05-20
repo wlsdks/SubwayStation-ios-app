347d2b82ee6ffc0aafb547de5a80929276685a4d
jinan <ja.choi@sweetk.co.kr>
2023-05-20 17:48:49 +0900

    1. StationDetailCollectionViewCell 코드추가 -> 셀 설정

--- SubwayStation-ios-app/Scenes/StationDetailCollectionViewCell.swift
+++ SubwayStation-ios-app/Scenes/StationDetailCollectionViewCell.swift
@@ -10,8 +10,45 @@ import UIKit
 
 final class StationDetailCollectionViewCell: UICollectionViewCell {
     
+    private lazy var lineLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12.0, weight: .bold)
+        
+        return label
+    }()
+    
+    private lazy var remainTimeLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 15.0, weight: .medium)
+        
+        return label
+    }()
+    
     func setup() {
-        backgroundColor = .gray
+        layer.cornerRadius = 12.0
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.2
+        layer.shadowRadius = 10
+        
+        // 이걸 해줘야 위의 layer 값들이 적용된다.(기준값이 필요)
+        backgroundColor = .systemBackground
+        
+        [lineLabel, remainTimeLabel].forEach { addSubview($0) }
+        
+        lineLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16.0)
+            $0.top.equalToSuperview().inset(16.0)
+        }
+        
+        remainTimeLabel.snp.makeConstraints {
+            $0.leading.equalTo(lineLabel)
+            $0.top.equalTo(lineLabel.snp.bottom).offset(16.0)
+            $0.bottom.equalToSuperview().inset(16.0)
+        }
+        
+        lineLabel.text = "한양대 방면"
+        remainTimeLabel.text = "뚝섬 도착"
+        
     }
     
 }
